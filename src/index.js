7928f4144a3b0770e8b18360ef8938156913c122
Don Maxito <maxharz@gmail.com>
2018-03-19 17:31:29 -0600

    export isObject

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import diff from './diff/index.js';
-import addedDiff from './added/index.js';
+/*import addedDiff from './added/index.js';
 import deletedDiff from './deleted/index.js';
 import updatedDiff from './updated/index.js';
-import detailedDiff from './detailed/index.js';
+import detailedDiff from './detailed/index.js';*/
+import { isObject } from './utils/index.js';
 
 export {
-  addedDiff,
   diff,
-  deletedDiff,
-  updatedDiff,
-  detailedDiff
+  isObject
 };
