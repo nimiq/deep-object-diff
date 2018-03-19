128ea6d553f37d109eedd905de3560b73d0465d2
sisou <hello@soerenschwert.de>
2018-03-19 00:54:10 -0600

    Enable diff of Maps

--- src/diff/index.js
+++ src/diff/index.js
@@ -1,9 +1,37 @@
-import { isDate, isEmpty, isObject, properObject } from '../utils/index.js';
+import { isMap, isDate, isEmpty, isObject, properObject } from '../utils/index.js';
 
 const diff = (lhs, rhs) => {
   if (lhs === rhs) return {}; // equal return no diff
 
   if (!isObject(lhs) || !isObject(rhs)) return rhs; // return updated rhs
+
+  if (isMap(rhs)) {
+    if (!isMap(lhs)) return rhs;
+
+    const diffMap = new Map();
+
+    for (const [key, value] of rhs) {
+      if (!lhs.has(key)) { // new key
+        diffMap.set(key, value);
+        continue;
+      }
+
+      const valueDiff = diff(lhs.get(key), value);
+      if (Object.keys(valueDiff).length) {
+        diffMap.set(key, valueDiff);
+      }
+    }
+
+    // Find deleted keys
+    for (const [key, value] of lhs) {
+      if(!rhs.has(key)) {
+        diffMap.set(key, undefined);
+      }
+    }
+
+    return diffMap.size > 0 ? diffMap : {};
+  }
+  else if (isMap(lhs)) return rhs;
 
   const l = properObject(lhs);
   const r = properObject(rhs);
@@ -22,7 +50,7 @@ const diff = (lhs, rhs) => {
 
     const difference = diff(l[key], r[key]);
 
-    if (isObject(difference) && isEmpty(difference) && !isDate(difference)) return acc; // return no diff
+    if (isObject(difference) && isEmpty(difference) && !isDate(difference) && !isMap(difference)) return acc; // return no diff
 
     return { ...acc, [key]: difference }; // return updated key
   }, deletedValues);
